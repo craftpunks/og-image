54638e35aae6a77dc8d3cbc718175183268581d9
Stéphane Depierrepont <toorop@gmail.com>
2022-01-22 10:33:48 +0100

    only base64 (& add headers for cdn cache and stale)

--- app.js
+++ app.js
@@ -8,29 +8,45 @@ const app = express()
 
 // ?debug pour afficher les couleurs
 // ?html pour afficher la version HTML avant conversion
+// ? sur text ça permet d'afficher une erreur plus parlante si pas de text
 
-app.get('/img/:text/:domain?/:b64?', async function(req, res) {
-    //const {text, template} = req.params
-
+app.get('/img/:text?/:footer?', async function(req, res) {
     const viewDebug = 'debug' in req.query
     const viewHTML = 'html' in req.query
-    const isBase64 = req.params.b64
-
-    let text = req.params.text || "T'as pas oublié un truc mec ?"
-    if (isBase64) text = Buffer.from(text, 'base64').toString()
-    text = text.trim().replaceAll('\n', '<br />')
-
-    let domain = req.params.domain || ''
-    if (isBase64) domain = Buffer.from(domain, 'base64').toString()
-
     const maxLength = 200
+
+    // check text
+    if (!req.params.text) {
+        res.status(400).send('Missing text parameter')
+        return
+    }
+    let text = req.params.text
+    // valid base64 ?
+    if (!isBase64(text)) {
+        res.status(400).send('Invalid base64 for text parameter')
+        return
+    }
+    text = atob(text).trim().replaceAll('\n', '<br />')
     if (text.length > maxLength) text = text.substring(0, maxLength) + '...'
+
+    // check footer
+    let footer = req.params.footer
+    if (footer) {
+        if (!isBase64(footer)) {
+            res.status(400).send('Invalid base64 for footer parameter')
+            return
+        }
+        footer = atob(footer).trim()
+        if (footer.length > maxLength) footer = footer.substring(0, maxLength) + '...'
+    } else {
+        footer = ''
+    }
 
     const raw = await getTemplate()
 
     const params = {
         text,
-        domain
+        footer
     }
 
     const html = compileTemplate(raw, params, {
@@ -46,7 +62,9 @@ app.get('/img/:text/:domain?/:b64?', async function(req, res) {
 
     res.writeHead(200, {
         'Content-Type': 'image/png',
-        'Cache-Control': 'max-age=86400'
+        'Cache-Control': 'max-age=86400, s-maxage=84000, stale-if-error=3600',
+        'Cloudflare-CDN-Cache-Control': 'max-age=24400, stale-if-error=3600',
+        'CDN-Cache-Control': '18000, stale-if-error=3600'
     })
 
     res.end(image, 'binary')
@@ -75,6 +93,15 @@ function compileTemplate(html, vars, options) {
     res = res.replaceAll('|body-css|', options.debug ? 'debug' : '')
 
     return res
+}
+
+// not perfect but enough for now
+function isBase64(str) {
+    try {
+        return btoa(atob(str)) == str;
+    } catch (err) {
+        return false;
+    }
 }
 
 app.listen(3333)
